b3e6a2ca48fd49046bcd481c5db5de602fbeab1e
Muhammad Habibullah <mhabibullah14@yahoo.com>
2020-07-18 13:15:30 +0700

    prob_5c

--- prob_5/prob_5.go
+++ prob_5/prob_5.go
@@ -1,5 +1,7 @@
 package prob5
 
+import "math"
+
 func gcd(a, b int) int {
 	if a == 0 {
 		return b
@@ -35,4 +37,56 @@ func ProblemFiveB(a, b int) int {
 	}
 
 	return i
+}
+
+// https://gist.github.com/lakshanwd/44c79820a6b9399afa3f56341c2825ef
+func nthPrime(limit int) int {
+	primes := make([]int, limit)
+	count := 0
+	isPrimeDivisible := func(candidate int) bool {
+		for j := 0; j < count; j++ {
+			if math.Sqrt(float64(candidate)) < float64(primes[j]) {
+				return false
+			}
+			if (candidate % primes[j]) == 0 {
+				return true
+			}
+		}
+		return false
+	}
+	for candidate := 2; ; {
+		if count < limit {
+			if !isPrimeDivisible(candidate) {
+				primes[count] = candidate
+				count++
+			}
+			candidate++
+		} else {
+			break
+		}
+	}
+	return primes[limit-1]
+}
+
+func ProblemFiveC(i, k int) (n int) {
+	n = 1
+	var check = true
+	limit := int(math.Sqrt(float64(k)))
+	pi := nthPrime(i)
+
+	for pi <= k {
+		ai := 1
+		if check {
+			if pi <= limit {
+				ai = int(math.Floor(math.Log(float64(k)) / math.Log(float64(pi))))
+			} else {
+				check = false
+			}
+		}
+		n *= int(math.Pow(float64(pi), float64(ai)))
+		i++
+		pi = nthPrime(i)
+	}
+
+	return
 }
--- prob_5/prob_5_test.go
+++ prob_5/prob_5_test.go
@@ -21,6 +21,7 @@ func TestProblemFive(t *testing.T) {
 	}{
 		"use lcm & gcd":     {inputOutput, ProblemFiveA},
 		"without lcm & gcd": {inputOutput, ProblemFiveB},
+		"use nth prime":     {inputOutput, ProblemFiveC},
 	}
 
 	for testName, ts := range testCases {
@@ -46,9 +47,19 @@ func BenchmarkProblemFiveB(b *testing.B) {
 	}
 }
 
+func BenchmarkProblemFiveC(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ProblemFiveC(1, 20)
+	}
+}
+
 //goos: linux
 //goarch: amd64
 //pkg: project-euler/prob_5
-//BenchmarkProblemFiveA-8   	  749502	      1583 ns/op
-//BenchmarkProblemFiveB-8   	 1222333	       973 ns/op
+//BenchmarkProblemFiveA
+//BenchmarkProblemFiveA-8   	 1562252	       700 ns/op
+//BenchmarkProblemFiveB
+//BenchmarkProblemFiveB-8   	 2218903	       539 ns/op
+//BenchmarkProblemFiveC
+//BenchmarkProblemFiveC-8   	  786739	      1346 ns/op
 //PASS
