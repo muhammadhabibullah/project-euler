91c498d52773608d636c14ff641ae498be1b76d3
Muhammad Habibullah <mhabibullah14@yahoo.com>
2020-07-26 18:03:31 +0700

    prob_6_test

--- prob_6/prob_6_test.go
+++ prob_6/prob_6_test.go
@@ -5,5 +5,48 @@ import (
 )
 
 func TestProblemSix(t *testing.T) {
+	inputOutputs := []struct {
+		input  int
+		output int
+	}{
+		{10, 2640},
+		{100, 25164150},
+		{1000, 250166416500},
+	}
 
+	solutions := map[string]func(int) int{
+		"brute force": ProblemSixA,
+		"arithmetic":  ProblemSixB,
+	}
+
+	for _, io := range inputOutputs {
+		for solutionName, solution := range solutions {
+			got := solution(io.input)
+			expected := io.output
+			if got != expected {
+				t.Errorf("%s solution function give wrong answer : %d; expected: %d", solutionName, got, expected)
+			}
+		}
+	}
 }
+
+func BenchmarkProblemFiveA(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ProblemSixA(100)
+	}
+}
+
+func BenchmarkProblemFiveB(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ProblemSixB(100)
+	}
+}
+
+//goos: linux
+//goarch: amd64
+//pkg: project-euler/prob_6
+//BenchmarkProblemFiveA
+//BenchmarkProblemFiveA-8   	20764658	        52.2 ns/op
+//BenchmarkProblemFiveB
+//BenchmarkProblemFiveB-8   	1000000000	         0.500 ns/op
+//PASS
